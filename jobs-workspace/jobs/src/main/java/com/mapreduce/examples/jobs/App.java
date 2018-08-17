6d2ccde89926b5db90d94d2abdf71908e995785f
Matheus Cunha <mathcunha@gmail.com>
2018-08-17 13:29:41 +0000

    adding job example

---
+++ jobs-workspace/jobs/src/main/java/com/mapreduce/examples/jobs/App.java
@@ -0,0 +1,13 @@
+package com.mapreduce.examples.jobs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
